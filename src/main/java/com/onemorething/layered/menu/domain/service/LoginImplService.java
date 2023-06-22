8f9fee06e03aaf033b1f1b06f4845c4cc9c6329b
myeonghwan cho <1jmhcho@gmail.com>
2023-06-22 17:33:48 +0900

    비밀번호 재입력 유효성 검증 기능 구현 #3

---
+++ src/main/java/com/onemorething/layered/menu/domain/service/LoginImplService.java
@@ -0,0 +1,22 @@
+package com.onemorething.layered.menu.domain.service;
+
+public class LoginImplService {
+
+    /* 설명. 비밀번호 재확인 로직*/
+    public String signUpCheckPassward(String pwd1, String pwd2) {
+
+        String result;
+
+        if (!(pwd1.trim().equals(pwd2))) {
+            result = "No";
+        } else {
+            result = "Yes";
+        }
+
+        return result;
+    }
+
+    /* 설명. 로그인 아이디 중복 체크
+     *   아이디 내부 특수 문자/ 대문자 있는 지 체크
+     * */
+}
