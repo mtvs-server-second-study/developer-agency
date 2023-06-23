7496df25ab1cd006428326b839afb1310d7ce580
myeonghwan cho <1jmhcho@gmail.com>
2023-06-23 19:18:22 +0900

    메인페이지 매핑, 회원가입 페이지 작성, 회원가입 페이지 입력값 넘기기 #18

--- src/main/java/com/onemorething/layered/menu/application/controller/AccountController.java
+++ src/main/java/com/onemorething/layered/menu/application/controller/AccountController.java
@@ -1,18 +1,58 @@
 package com.onemorething.layered.menu.application.controller;
 
+import com.onemorething.layered.menu.application.dto.MemberDTO;
+import com.onemorething.layered.menu.application.service.SignUpService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/account/*")
 public class AccountController {
 
-    @GetMapping("signup")
+    private final SignUpService signUpService;
+
+    @Autowired
+    public AccountController(SignUpService signUpService) {
+
+        this.signUpService = signUpService;
+    }
+
+    @GetMapping("/signup")
     public String signUp() {
 
         return "account/signup";
     }
+
+    /* 회원가입에서 입력 값 넘기는 매핑 */
+    @PostMapping("/signup")
+    public String signUpMenu(Model model, WebRequest request, MemberDTO memberDTO) {
+        // 사용자 입력값 변수에 담기
+        String email = request.getParameter("email");
+        String name = request.getParameter("name");
+        String pwd1 = request.getParameter("pwd1");
+        String pwd2 = request.getParameter("pwd2");
+        String phone = request.getParameter("phone");
+
+        // memberDTO 에 값 담기
+        memberDTO.setUserEmail(email);
+        memberDTO.setUserName(name);
+        memberDTO.setUserPwd(pwd1);
+        memberDTO.setUserCheckPwd(pwd2);
+        memberDTO.setUserPhone(phone);
+
+        // 서비스 호출
+        signUpService.SignUp(memberDTO);
+
+        // 어떤걸 리턴해야 하는가?
+        return "redirect:/";
+    }
+
     @GetMapping("login")
     public String login() {
 
--- src/main/java/com/onemorething/layered/menu/application/service/SignUpService.java
+++ src/main/java/com/onemorething/layered/menu/application/service/SignUpService.java
@@ -1,4 +1,36 @@
 package com.onemorething.layered.menu.application.service;
 
+import com.onemorething.layered.menu.application.dto.MemberDTO;
+import com.onemorething.layered.menu.domain.service.UserSignUpService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class SignUpService {
+
+    // repo 의존성 주입하기
+    private final UserSignUpService userSignUpService;
+
+    @Autowired
+    public SignUpService(UserSignUpService userSignUpService) {
+
+        this.userSignUpService = userSignUpService;
+    }
+
+    /* 회원가입 로직 */
+    public void SignUp(MemberDTO memberDTO) {
+
+        System.out.println("memberDTO = " + memberDTO);
+
+        //검증로직
+        userSignUpService.validEmail(memberDTO.getUserEmail());
+        userSignUpService.signUpValidPwd(memberDTO.getUserPwd());
+        userSignUpService.signUpCheckPassword(memberDTO.getUserPwd() ,memberDTO.getUserCheckPwd());
+        userSignUpService.signUpValidPhone(memberDTO.getUserPhone());
+        userSignUpService.signUpValidPhone(memberDTO.getUserPhone());
+
+        // eneity 변환
+
+        // repo 호출하여 저장
+    }
 }
