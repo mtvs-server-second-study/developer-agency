aff7d368cd7ff44ffb293b47287ba84ded474889
UncleSubak <kko6369@naver.com>
2023-06-25 02:12:04 +0900

    feat: mybatis/mySQL 연동 성공, 값 넘어오는 지 확인 완료. 공식문서 보고 resultMap 추가

---
+++ src/main/java/com/onemorething/layered/menu/domain/aggregate/entity/User.java
@@ -0,0 +1,56 @@
+package com.onemorething.layered.menu.domain.aggregate.entity;
+
+public class User {
+
+    private String userEmail;
+    private String userName;
+    private String userPwd;
+    private String userBirth;
+    private String userPhone;
+    private String userTechStack1;
+    private String userTechStack2;
+    private String userTechStack3;
+
+    public User(String userEmail, String userName, String userPwd, String userBirth, String userPhone, String userTechStack1, String userTechStack2, String userTechStack3) {
+        this.userEmail = userEmail;
+        this.userName = userName;
+        this.userPwd = userPwd;
+        this.userBirth = userBirth;
+        this.userPhone = userPhone;
+        this.userTechStack1 = userTechStack1;
+        this.userTechStack2 = userTechStack2;
+        this.userTechStack3 = userTechStack3;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserPwd() {
+        return userPwd;
+    }
+
+    public String getUserBirth() {
+        return userBirth;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public String getUserTechStack1() {
+        return userTechStack1;
+    }
+
+    public String getUserTechStack2() {
+        return userTechStack2;
+    }
+
+    public String getUserTechStack3() {
+        return userTechStack3;
+    }
+}
--- src/main/java/com/onemorething/layered/menu/domain/repository/UserRepository.java
+++ src/main/java/com/onemorething/layered/menu/domain/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.onemorething.layered.menu.domain.repository;
 
+import com.onemorething.layered.menu.domain.aggregate.entity.User;
+
+import java.util.List;
+
 public interface UserRepository {
+
+    public List<User> getUserList();
 }
