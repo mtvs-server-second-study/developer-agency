e794024a9fdafda7164c13af16fba64622c3718a
myeonghwan cho <1jmhcho@gmail.com>
2023-06-24 15:51:03 +0900

    글쓰기 페이지를 컨트롤러에서 서비스까지 연결하기 #21

---
+++ src/main/java/com/onemorething/layered/menu/application/controller/ContentsController.java
@@ -0,0 +1,19 @@
+package com.onemorething.layered.menu.application.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/contents/*")
+public class ContentsController {
+
+    // 게시글 및 상세페이지 관련 service 의존성 주입하기
+
+    @GetMapping("/writing")
+    public String writing() {
+
+        return "contents/writing";
+    }
+}
