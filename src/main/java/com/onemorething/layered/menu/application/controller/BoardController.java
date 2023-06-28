5ab5b229fee392523fa02f3dd50a6cdad1763ef0
myeonghwan cho <1jmhcho@gmail.com>
2023-06-28 13:35:42 +0900

    refactor: 프로필 이미지, 깃허브 잔디 이미지 저장 경로 변경 #73

--- src/main/java/com/onemorething/layered/menu/application/controller/BoardController.java
+++ src/main/java/com/onemorething/layered/menu/application/controller/BoardController.java
@@ -3,6 +3,7 @@ package com.onemorething.layered.menu.application.controller;
 import com.onemorething.layered.menu.application.dto.BoardDTO;
 import com.onemorething.layered.menu.application.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.WebProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -38,15 +39,11 @@ public class BoardController {
                              @RequestParam("git-contribution") MultipartFile gitContribution,
                              Model model,
                              HttpSession session) {
-//        최종 병합 시 삭제
-//        System.out.println("profile = " + profile);
-//        System.out.println("salary = " + salary);
-//        System.out.println("title = " + title);
-//        System.out.println("threePoem = " + threePoem);
-//        System.out.println("gitContribution = " + gitContribution);
 
+        // 파일 저장 루트 가져오기
+        String root = new File("src/main/resources/static").getAbsolutePath();
 
-        String root = System.getProperty("user.dir");   // 현재 실행 중인 파일의 작업 디렉토리를 가져옴
+        // 현재 실행 중인 파일의 작업 디렉토리를 가져옴
         String filePath = root + "/profileImg";
         String filePath2 = root + "/gitContribution";
 
@@ -62,23 +59,19 @@ public class BoardController {
 
         String originFileName = profile.getOriginalFilename();
         String originFileName2 = gitContribution.getOriginalFilename();
-//        System.out.println("originFileName = " + originFileName);
-//        System.out.println("originFileName2 = " + originFileName2);
 
         String ext = originFileName.substring(originFileName.lastIndexOf("."));
         String ext2 = originFileName2.substring(originFileName2.lastIndexOf("."));
-//        System.out.println("ext = " + ext);
-//        System.out.println("ext2 = " + ext2);
 
         String savedName = UUID.randomUUID().toString().replaceAll("-", "") + ext;
         String savedName2 = UUID.randomUUID().toString().replaceAll("-", "") + ext2;
 
-        String path = filePath + "/" + savedName;
-        String path2 = filePath2 + "/" + savedName2;
+        String path = "/profileImg" + "/" + savedName;
+        String path2 = "/gitContribution" + "/" + savedName2;
 
         try {
-            profile.transferTo(new File(path));
-            gitContribution.transferTo(new File(path2));
+            profile.transferTo(new File(filePath + "/" + savedName));
+            gitContribution.transferTo(new File(filePath2 + "/" + savedName2));
             model.addAttribute("message", "게시글이 저장 되었습니다.");
 
             BoardDTO boardDTO = new BoardDTO((String) session.getAttribute("userEmail"), title, threePoem, salary, path, path2);
